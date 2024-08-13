b443e17ab6da3f829d18cc7edbaf15f2aecc4d05
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-13 10:38:44 +0530

    new about php

---
+++ Arthmethic.php
@@ -0,0 +1,22 @@
+<!-- my first php script -->
+<?php
+echo "Hello, World!"."<br>";
+
+$a = 5;
+$b = 2;
+
+$sum = $a + $b;
+echo "Sum: " . $sum . "<br>";
+
+$diff = $a - $b;
+echo "Difference: " . $diff . "<br>";
+
+$product = $a * $b;
+echo "Product: ". $product ."<br>";
+
+$quotient = $a / $b;
+echo "Quotient: " . $quotient . "<br>";
+
+$power = pow($a, $b);
+echo "Power: " . $power . "<br>";
+?>
