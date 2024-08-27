ff05bf83558c35739c3a37314b1fec33efecbfe6
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-27 10:29:46 +0530

    ConstPHP

--- const.php
+++ const.php
@@ -3,9 +3,34 @@ const x= 45;
 const y=78;
 function add(){
     $result =x+y;
-    echo "Sum is:  " . $result;
+    echo "Sum is:  " . $result ."<br>";
 }
-add(x,y);
+add(x,y) ;
 
 
+?>
+
+<?php
+$b=4 | 2 & 1;
+
+echo $b . "<br>";
+?>
+
+
+<?php
+$a=5;
+$c=10;
+$d=2;
+$result=$a+$c*$c >= 20 ? $a++ : --$c + $c;
+echo $result . "<br>";
+?>
+
+
+<?php
+$q=4;
+$w=2;
+$r=3;
+
+$e=$q+$w*$r**$w/$q-$w%$r;
+echo $e;
 ?>
