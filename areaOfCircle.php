7293e1f22a3d8f511e59f0475010d96bbc1ec019
Gourav Chaudhary <gouravsittam@gmail.com>
2024-08-27 09:39:16 +0530

    ConstPHP

--- areaOfCircle.php
+++ areaOfCircle.php
@@ -1,16 +1,15 @@
-
 <?php
 $r = 5; 
-echo "Area of Circle with Radius $r is ", round(M_PI * pow($r, 2)), "<br>";
+echo "Area of Circle with Radius $r is " . round(M_PI * pow($r, 2)) . "<br>";
 ?>
 
 <?php
-define('PI', 3.1415926535897932384626433832795);
-define("GRAVITY", 9.8);
+define('PI', 3.14);
+define('GRAVITY', 9.8);
 
-$radius=5;
-$cir=2*PI*$radius . "<br>";
+$radius = 5;
+$cir = 2 * PI * $radius;
 
-echo"Example 1: circumference of a circle with radius $radius is $cir\n";
-echo"Gravity on Earth is ". GRAVITY . "m/s<br>";
+echo "Example 1: Circumference of a circle with radius $radius is " . number_format($cir, 2) . "<br>";
+echo "Gravity on Earth is " . GRAVITY . " m/s²<br>";
 ?>
