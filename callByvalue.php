8e25ef4ae83683fa04bc71c90a4e9b968d0bd4ec
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-05 15:18:19 +0530

    php

---
+++ callByvalue.php
@@ -0,0 +1,12 @@
+<?php
+function chnage_name($nm){
+    echo"Initially the name $nm \n";
+    $nm=$nm."_new";
+    echo"The functions change tyhe name to $nm \n";
+}
+$name ="john";
+echo "my name is $name \n";
+chnage_name($name);
+echo"my name is still $name";
+
+?>
