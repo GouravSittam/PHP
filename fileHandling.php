30d48d551c0a3ca1e5b641f0766812a70a4c8dec
Gourav Chaudhary <gouravsittam@gmail.com>
2024-09-19 14:33:02 +0530

    upload

--- fileHandling.php
+++ fileHandling.php
@@ -6,13 +6,22 @@
     <title>Document</title>
 </head>
 <body>
-    <form action=" " method="post" enctype="multipart/form-data"></form>
-    Upload the File : <input type="file" name="myfile">
-    <br>
-    <input type="submit" value="Upload">
+    <form action="" method="post" enctype="multipart/form-data">
+        Upload the File: <input type="file" name="myfile">
+        <br>
+        <input type="submit" value="Upload">
+    </form>
 
     <?php
-    print_r($_FILES["myfile"]);
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_FILES["myfile"]) && $_FILES["myfile"]["error"] == 0) {
+            print_r($_FILES["myfile"]);
+            $file_name = $_FILES["myfile"]["name"]."<br>";
+            $file_size = $_FILES["myfile"]["size"]."<br>";
+            $file_type = $_FILES["myfile"]["type"]."<br>";
+            $file_tempname = $_FILES["myfile"]["tmp_name"]."<br>";
+        }
+    }
     ?>
 </body>
 </html>
